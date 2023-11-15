04014d1b306caef39cabdb1634e4ea501a2a7729
Rafael <rafaelmagalhaes1@live.com>
2023-11-15 16:16:51 -0300

    feat: add browser router and global style on main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { GlobalStyle } from './styles/GlobalStyle.tsx';
 import App from './App.tsx';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as Element).render(
-  <React.StrictMode>
+  <BrowserRouter>
+    <GlobalStyle />
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
 );
