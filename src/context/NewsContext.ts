1c4b3b3b88dfa60a7b224c35f398c72685ca04b0
Rafael <rafaelmagalhaes1@live.com>
2023-11-18 15:36:25 -0300

    feat: add filters on NewsProvider

--- src/context/NewsContext.ts
+++ src/context/NewsContext.ts
@@ -2,7 +2,12 @@ import { createContext } from 'react';
 import { NewsType } from '../types/types';
 
 type NewsContextType = {
-  news: NewsType[];
+  dataNews: NewsType[];
+  filteredNews: NewsType[];
+  filterByString: (searchString: string) => void;
+  filterByNoticia: () => void;
+  filterByRelease: () => void;
+  filterByRecent: () => void;
 };
 
 export const NewsContext = createContext({} as NewsContextType);
--- src/context/NewsProvider.tsx
+++ src/context/NewsProvider.tsx
@@ -1,15 +1,32 @@
 import { NewsContext } from './NewsContext';
 import useFetchApi from '../hooks/useFetchApi';
+import useFilterNews from '../hooks/useFilterNews';
 
 type NewsProviderProps = {
   children: React.ReactNode;
 };
 
 function NewsProvider({ children } : NewsProviderProps) {
-  const news = useFetchApi();
+  const { dataNews } = useFetchApi();
+  const {
+    filteredNews,
+    filterByString,
+    filterByNoticia,
+    filterByRelease,
+    filterByRecent,
+  } = useFilterNews(dataNews);
+
+  const value = {
+    dataNews,
+    filteredNews,
+    filterByString,
+    filterByNoticia,
+    filterByRelease,
+    filterByRecent,
+  };
 
   return (
-    <NewsContext.Provider value={ news }>
+    <NewsContext.Provider value={ value }>
       {children}
     </NewsContext.Provider>
   );
