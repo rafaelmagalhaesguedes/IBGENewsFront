8aa12b72379c28c0fbe3e5e0ef1fc2b585ac3985
Rafael <rafaelmagalhaes1@live.com>
2023-11-18 06:16:15 -0300

    fix: news type

---
+++ src/types/types.ts
@@ -0,0 +1,14 @@
+export type NewsType = {
+  data_publicacao: string;
+  destaque: boolean;
+  editorias: string;
+  id: number;
+  imagens: string;
+  introducao: string;
+  link: string;
+  produto_id: number;
+  produtos: string;
+  produtos_relacionados: string;
+  tipo: string;
+  titulo: string;
+};
