cf3ceede3b78ba9138bd46a760ac84ba95179113
Rafael <rafaelmagalhaes1@live.com>
2023-11-18 06:15:51 -0300

    feat: create styles to App

---
+++ src/styles/AppStyles.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const AppContainer = styled.div`
+  width: 100%;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #F1F2F3;
+`;
+
+export const Main = styled.main`
+  width: 90%;
+`;
