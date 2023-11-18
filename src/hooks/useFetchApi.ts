4441a315a46475034f614a379395f26c2af90283
Rafael <rafaelmagalhaes1@live.com>
2023-11-18 15:32:49 -0300

    fix: rename variable news to dataNews

--- src/hooks/useFetchApi.ts
+++ src/hooks/useFetchApi.ts
@@ -2,18 +2,18 @@ import { useEffect, useState } from 'react';
 import { NewsType } from '../types/types';
 
 function useFetchApi() {
-  const [news, setNews] = useState<NewsType[]>([]);
+  const [dataNews, setDataNews] = useState<NewsType[]>([]);
 
   useEffect(() => {
     const fetchNews = async () => {
       const response = await fetch('http://servicodados.ibge.gov.br/api/v3/noticias/?qtd=100');
       const data = await response.json();
-      setNews(data.items);
+      setDataNews(data.items);
     };
     fetchNews();
   }, []);
 
-  return { news };
+  return { dataNews };
 }
 
 export default useFetchApi;
