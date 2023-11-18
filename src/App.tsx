46b8a94867f7955fbe02409852ad1ed52106c529
Rafael <rafaelmagalhaes1@live.com>
2023-11-18 06:12:06 -0300

    refact: App container

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import { Route, Routes } from 'react-router-dom';
-import Layout from './components/Layout/Layout';
-import Home from './pages/Home/Home';
-import News from './pages/News/News';
+import Header from './components/Header/Header';
+import NewsList from './components/NewsList/NewsList';
+import { AppContainer, Main } from './styles/AppStyles';
+import LatestNews from './components/LatestNews/LatestNews';
+import FilterNews from './components/FilterNews/FilterNews';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={ <Layout /> }>
-        <Route index element={ <Home /> } />
-        <Route path="/news" element={ <News /> } />
-      </Route>
-    </Routes>
+    <AppContainer>
+      <Header />
+      <Main>
+        <LatestNews />
+        <FilterNews />
+        <NewsList />
+      </Main>
+    </AppContainer>
   );
 }
 
