75813b2d88585cabf57c7bd8ed50e573ac5e828f
Rafael <rafaelmagalhaes1@live.com>
2023-11-18 06:13:30 -0300

    fix: add NewsProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { GlobalStyle } from './styles/GlobalStyle.tsx';
 import App from './App.tsx';
+import NewsProvider from './context/NewsProvider.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as Element).render(
-  <BrowserRouter>
-    <GlobalStyle />
-    <App />
-  </BrowserRouter>,
+  <NewsProvider>
+    <BrowserRouter>
+      <GlobalStyle />
+      <App />
+    </BrowserRouter>
+  </NewsProvider>,
 );
