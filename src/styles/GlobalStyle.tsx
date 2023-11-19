c5dcaca4c6ea7fd4d0968f8f6899d8777c95f233
Rafael <rafaelmagalhaes1@live.com>
2023-11-19 12:42:33 -0300

    feat: add google fonts

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -1,11 +1,19 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
+
+  @import url('https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,300;0,400;0,500;0,700;0,800;0,1000;1,200;1,400&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Serif:wght@600&family=Nunito:ital,wght@0,300;0,400;0,500;0,700;0,800;0,1000;1,200;1,400&display=swap');
+  
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: Gabarito , sans-serif;
+    font-family: Nunito , sans-serif;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-family: IBM Plex Serif , serif;
   }
 
   a {
