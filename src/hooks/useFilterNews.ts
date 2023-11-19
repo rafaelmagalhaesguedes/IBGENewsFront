f98a8638bd36f7848969bd4090b9d1e16d88c893
Rafael <rafaelmagalhaes1@live.com>
2023-11-19 12:42:18 -0300

    feat: add filterByFavorites

--- src/hooks/useFilterNews.ts
+++ src/hooks/useFilterNews.ts
@@ -5,19 +5,20 @@ import { NewsType } from '../types/types';
 const useFilterNews = (news: NewsType[]) => {
   const [filteredNews, setFilteredNews] = useState<NewsType[]>([]);
 
+  // Set filtered news to news when news is updated
   useEffect(() => {
     if (filteredNews.length === 0) {
       setFilteredNews(news);
     }
   }, [filteredNews, news]);
 
+  // Filter by string
   const filterByString = useCallback((search: string) => {
     const filtered = news.filter(({ titulo, destaque }) => {
       const byTitle = titulo.toLowerCase().includes(search.toLowerCase());
       const byDestaque = destaque.toString().toLowerCase().includes(search.toLowerCase());
       return byTitle || byDestaque;
     });
-    console.log(filtered);
     if (filtered.length === 0) {
       Swal.fire({
         title: 'Nenhuma notícia encontrada!',
@@ -29,20 +30,31 @@ const useFilterNews = (news: NewsType[]) => {
     setFilteredNews(filtered);
   }, [news]);
 
+  // Filter by recent
   const filterByRecent = useCallback(() => {
     const sortedNews = [...news].sort((a, b) => (new Date(b.data_publicacao)
       .getTime() - new Date(a.data_publicacao).getTime()));
     setFilteredNews(sortedNews);
   }, [news]);
 
+  // Filter by type noticia
   const filterByNoticia = useCallback(() => {
     const filtered = news.filter(({ tipo }) => tipo === 'Notícia');
     setFilteredNews(filtered);
   }, [news]);
 
+  // Filter by type release
   const filterByRelease = useCallback(() => {
     const filtered = news.filter(({ tipo }) => tipo === 'Release');
     console.log(filtered);
+    setFilteredNews(filtered);
+  }, [news]);
+
+  // Filter by favorites
+  const filterByFavorites = useCallback(() => {
+    const favorites = JSON.parse(localStorage.getItem('favorites') || '[]');
+    const filtered = news.filter((newsItem) => favorites
+      .some((favorite: NewsType) => favorite.id === newsItem.id));
     setFilteredNews(filtered);
   }, [news]);
 
@@ -52,6 +64,7 @@ const useFilterNews = (news: NewsType[]) => {
     filterByRecent,
     filterByNoticia,
     filterByRelease,
+    filterByFavorites,
   };
 };
 
