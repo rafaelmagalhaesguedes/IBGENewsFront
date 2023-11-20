da9a5e0ec2057dcf841f5081cbd1bda9a7380f4e
Rafael <rafaelmagalhaes1@live.com>
2023-11-20 12:43:41 -0300

    refact: favorite button, change icons

--- src/components/FavoriteButton/FavoriteButton.tsx
+++ src/components/FavoriteButton/FavoriteButton.tsx
@@ -1,7 +1,8 @@
-import { FaHeart, FaRegHeart } from 'react-icons/fa';
 import { FavButton } from './Styles';
 import useFavorites from '../../hooks/useFavorites';
 import { NewsType } from '../../types/types';
+import favorited from '../../assets/images/blackHeartIcon.svg';
+import noFavorited from '../../assets/images/whiteHeartIcon.svg';
 
 type FavoriteButtonProps = {
   item: NewsType;
@@ -11,8 +12,12 @@ function FavoriteButton({ item } : FavoriteButtonProps) {
   const { isFavorited, handleFavoriteClick } = useFavorites(item);
 
   return (
-    <FavButton onClick={ handleFavoriteClick }>
-      {isFavorited ? <FaHeart size={ 25 } /> : <FaRegHeart size={ 25 } />}
+    <FavButton data-testid="favorite-button" onClick={ handleFavoriteClick }>
+      <img
+        src={ isFavorited ? favorited : noFavorited }
+        alt={ isFavorited ? 'favorited' : 'noFavorited' }
+        data-testid="favorite-btn"
+      />
     </FavButton>
   );
 }
