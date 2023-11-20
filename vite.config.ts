57b85bbe23751a721b849376ecd8beb6bb43f583
Rafael <rafaelmagalhaes1@live.com>
2023-11-20 12:34:43 -0300

    feat: install RTL and Vitest for tests, install Moment for date formated

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/// <reference types="vitest" />
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+// Se na criação do projeto foi criado com swc, altere a biblioteca para @vitejs/plugin-react-swc 
+
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './setupTests.ts',
+    css: true,
+    reporters: ['verbose'],
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      provider: 'v8'
+    }
+  },
 })
