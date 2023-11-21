06f956eaa06eba228522cfdd1833c082599fbcee
Rafael <rafaelmagalhaes1@live.com>
2023-11-21 11:29:33 -0300

    feat: create tests to useFetch hook

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,23 +1,35 @@
-import { render, screen } from '@testing-library/react';
-import App from '../App';
+import { render } from '@testing-library/react';
+import useFetch from '../hooks/useFetch';
 
-describe('App', () => {
-  it('renders input search', () => {
-    render(<App />);
-    const searchInput = screen.getByTestId('search-input');
-    expect(searchInput).toBeInTheDocument();
+// Crie um componente de teste que usa o hook useFetch
+function TestComponent() {
+  const { dataNews, isLoading } = useFetch();
+  if (isLoading) return <div>Loading...</div>;
+  return (
+    <div>
+      {dataNews.map((news) => <div key={ news.id }>{news.titulo}</div>)}
+    </div>
+  );
+}
+
+describe('UseFetch hook testing', () => {
+  it('1. Verify that the expected text appears in the document', async () => {
+    global.fetch = async () => new Response(JSON.stringify({ items: [{ id: 1, titulo: 'Test News' }] }));
+
+    const { findByText } = render(<TestComponent />);
+
+    // Verifique se o texto esperado aparece no documento
+    expect(await findByText('Test News')).toBeTruthy();
   });
 
-  it('renders filters button', () => {
-    render(<App />);
-    const recentNewsBtn = screen.getByText(/mais recentes/i);
-    const releaseBtn = screen.getByText(/release/i);
-    const newsBtn = screen.getByText(/notícia/i);
-    const favoritesBtn = screen.getByText(/favoritas/i);
+  it('2. Verify fetch error', async () => {
+    global.fetch = async () => {
+      throw new Error('Erro ao buscar dados da API');
+    };
 
-    expect(recentNewsBtn).toBeInTheDocument();
-    expect(releaseBtn).toBeInTheDocument();
-    expect(newsBtn).toBeInTheDocument();
-    expect(favoritesBtn).toBeInTheDocument();
+    const { findByText } = render(<TestComponent />);
+
+    // Verifique se o texto de carregamento aparece no documento
+    expect(await findByText('Loading...')).toBeTruthy();
   });
 });
