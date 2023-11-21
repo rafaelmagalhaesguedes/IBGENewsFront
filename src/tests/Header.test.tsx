a0492f8169b1c020f8a914432c2401b90679914a
Rafael <rafaelmagalhaes1@live.com>
2023-11-21 11:31:17 -0300

    refact: header tests

--- src/tests/Header.test.tsx
+++ src/tests/Header.test.tsx
@@ -1,23 +1,84 @@
-import { fireEvent, screen } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import { vi } from 'vitest';
-import { NewsContext } from '../context/NewsContext';
 import Header from '../components/Header/Header';
-import renderWithRouter from '../helpers/renderWithRouter';
+import { NewsContext } from '../context/NewsContext';
 
-const mockContextValue = {
-  dataNews: [],
-  filteredNews: [],
-  filterByString: vi.fn(),
-  filterByRecent: vi.fn(),
-  filterByRelease: vi.fn(),
-  filterByNoticia: vi.fn(),
-  filterByFavorites: vi.fn(),
-};
+// Definindo constantes para as literais
+const SEARCH_INPUT = 'search-input';
+const SEARCH_ICON = 'search-icon';
+const LOGO = 'logo';
 
-describe('Header', () => {
-  const SEARCH_INPUT = 'search-input';
-  it('renders correctly', () => {
-    renderWithRouter(
+describe('Header component testing', () => {
+  let filterNewsRefMock: React.RefObject<HTMLDivElement>;
+  let filterByStringMock: any;
+  let mockContextValue: any;
+
+  beforeEach(() => {
+    filterNewsRefMock = { current: { scrollIntoView: vi.fn() } as any };
+    filterByStringMock = vi.fn();
+
+    mockContextValue = {
+      dataNews: [],
+      isLoading: false,
+      filteredNews: [],
+      filterByNoticia: vi.fn(),
+      filterByString: filterByStringMock,
+      filterByData: vi.fn(),
+      filterByFonte: vi.fn(),
+      clearFilters: vi.fn(),
+    };
+  });
+
+  it('1. Renders correctly', () => {
+    const { getByTestId } = render(
+      <NewsContext.Provider value={ mockContextValue }>
+        <Header filterNewsRef={ filterNewsRefMock } />
+      </NewsContext.Provider>,
+    );
+
+    expect(getByTestId(LOGO)).toBeInTheDocument();
+    expect(getByTestId(SEARCH_INPUT)).toBeInTheDocument();
+    expect(getByTestId(SEARCH_ICON)).toBeInTheDocument();
+  });
+
+  it('2. Handles search input change', () => {
+    const { getByTestId } = render(
+      <NewsContext.Provider value={ mockContextValue }>
+        <Header filterNewsRef={ filterNewsRefMock } />
+      </NewsContext.Provider>,
+    );
+
+    fireEvent.change(getByTestId(SEARCH_INPUT), { target: { value: 'test' } });
+
+    expect((getByTestId(SEARCH_INPUT) as HTMLInputElement).value).toBe('test');
+  });
+
+  it('3. Handles search icon click', () => {
+    const { getByTestId } = render(
+      <NewsContext.Provider value={ mockContextValue }>
+        <Header filterNewsRef={ filterNewsRefMock } />
+      </NewsContext.Provider>,
+    );
+
+    fireEvent.click(getByTestId(SEARCH_ICON));
+
+    expect(filterByStringMock).toHaveBeenCalled();
+  });
+
+  it('4. Handles Enter key press', () => {
+    const { getByTestId } = render(
+      <NewsContext.Provider value={ mockContextValue }>
+        <Header filterNewsRef={ filterNewsRefMock } />
+      </NewsContext.Provider>,
+    );
+
+    fireEvent.keyPress(getByTestId(SEARCH_INPUT), { key: 'Enter', code: 13, charCode: 13 });
+
+    expect(filterByStringMock).toHaveBeenCalled();
+  });
+
+  it('5. Renders correctly', () => {
+    render(
       <NewsContext.Provider value={ mockContextValue }>
         <Header filterNewsRef={ { current: null } } />
       </NewsContext.Provider>,
@@ -32,8 +93,8 @@ describe('Header', () => {
     expect(screen.getByPlaceholderText('Buscar notícia')).toBeInTheDocument();
   });
 
-  it('calls the filterByString function when the search icon is clicked', () => {
-    renderWithRouter(
+  it('6. Calls the filterByString function when the search icon is clicked', () => {
+    render(
       <NewsContext.Provider value={ mockContextValue }>
         <Header filterNewsRef={ { current: null } } />
       </NewsContext.Provider>,
@@ -41,20 +102,6 @@ describe('Header', () => {
 
     fireEvent.change(screen.getByTestId(SEARCH_INPUT), { target: { value: 'test' } });
     fireEvent.click(screen.getByTestId('search-icon'));
-
-    expect(mockContextValue.filterByString).toHaveBeenCalledWith('test');
-  });
-
-  it('calls filterByString with input value when Enter is pressed', () => {
-    renderWithRouter(
-      <NewsContext.Provider value={ mockContextValue }>
-        <Header filterNewsRef={ { current: null } } />
-      </NewsContext.Provider>,
-    );
-
-    const input = screen.getByTestId(SEARCH_INPUT);
-    fireEvent.change(input, { target: { value: 'test' } });
-    fireEvent.keyPress(input, { key: 'Enter', code: 'Enter' });
 
     expect(mockContextValue.filterByString).toHaveBeenCalledWith('test');
   });
