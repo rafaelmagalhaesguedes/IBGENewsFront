58e03d4dc9bec268aa1f4f92ba44f9f43f1f535a
Rafael <rafaelmagalhaes1@live.com>
2023-11-23 09:17:34 -0300

    fix: add coments

--- src/helpers/formatDate.ts
+++ src/helpers/formatDate.ts
@@ -1,6 +1,10 @@
 import { formatDistanceToNow, parse } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 
+//
+// Função que recebe uma string com a data no formato dd/MM/yyyy HH:mm:ss
+// e retorna a data formatada
+//
 export const formatDate = (dateString: string) => {
   const date = parse(dateString, 'dd/MM/yyyy HH:mm:ss', new Date());
   return formatDistanceToNow(date, { addSuffix: true, locale: ptBR });
