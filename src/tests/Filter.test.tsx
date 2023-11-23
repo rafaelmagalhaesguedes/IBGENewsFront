a4c18be3346205cf834f4ca02baff1b836b62084
Rafael <rafaelmagalhaes1@live.com>
2023-11-23 12:23:23 -0300

    feat: create tests to filterByOldest and refact tests to filterByFavorites

--- src/tests/Filter.test.tsx
+++ src/tests/Filter.test.tsx
@@ -1,7 +1,6 @@
 import { renderHook } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
 import { vi } from 'vitest';
-import moment from 'moment';
 import { NewsType } from '../types/types';
 import useFilter from '../hooks/useFilter';
 
@@ -22,8 +21,6 @@ const news: NewsType[] = [
     link: 'http://agenciadenoticias.ibge.gov.br/agencia-noticias/2012-agencia-de-noticias/noticias/38398-encontro-de-servidores-do-ibge-termina-com-entrega-de-12-diretrizes-para-o-90-aniversario-do-instituto.html',
   },
 ];
-
-const DATA = 'DD/MM/YYYY HH:mm:ss';
 
 describe('Filter hook testing', () => {
   it('1. FilterByString testing', () => {
@@ -91,6 +88,18 @@ describe('Filter hook testing with localStorage', () => {
     expect(result.current.filteredNews).toEqual([news[0]]);
   });
 
+  it('3. FilterByFavorites should return an empty array when localStorage is empty', () => {
+    localStorage.clear();
+
+    const { result } = renderHook(() => useFilter(news));
+
+    act(() => {
+      result.current.filterByFavorites();
+    });
+
+    expect(result.current.filteredNews).toEqual([]);
+  });
+
   it('2. FilterByFavorites should filter news by favorites', () => {
     const mockFavorites = [news[0]];
     vi.spyOn(Storage.prototype, 'getItem').mockImplementation(() => JSON.stringify(mockFavorites));
@@ -105,36 +114,53 @@ describe('Filter hook testing with localStorage', () => {
   });
 });
 
-describe('Filter hook testing with moment filter by oldest news', () => {
-  it('1. FilterByOldest should sort news by date in ascending order when ascending is true', () => {
-    const { result } = renderHook(() => useFilter(news));
+describe('Filter hook testing with moment filter by recent news', () => {
+  const mock = {
+    items: [
+      {
+        id: 38398,
+        tipo: 'Notícia',
+        titulo: 'Encontro de servidores do IBGE termina com entrega de 12 diretrizes para o 90º aniversário do Instituto',
+        introducao: 'No Encontro Diálogos IBGE 90 anos, realizado nos dias 16 e 17 de novembro na Unidade de Parada Lucas (RJ), foram escolhidas 12 diretrizes que vão nortear os próximos 3 anos do IBGE - Foto: Licia Rubinstein\\Agência IBGE Notícias O IBGE encerrou, no fim da...',
+        data_publicacao: '17/11/2023 20:44:00',
+        produto_id: 0,
+        produtos: '',
+        editorias: 'ibge',
+        imagens: '{"image_intro":"images\\/agenciadenoticias\\/ibge\\/2023_11\\/Encerramento_THUMB.jpg","float_intro":"","image_intro_alt":"","image_intro_caption":"","image_fulltext":"images\\/agenciadenoticias\\/ibge\\/2023_11\\/Encerramento_HOME.jpg","float_fulltext":"","image_fulltext_alt":"","image_fulltext_caption":""}',
+        produtos_relacionados: '',
+        destaque: true,
+        link: 'http://agenciadenoticias.ibge.gov.br/agencia-noticias/2012-agencia-de-noticias/noticias/38398-encontro-de-servidores-do-ibge-termina-com-entrega-de-12-diretrizes-para-o-90-aniversario-do-instituto.html',
+      },
+      {
+        id: 38397,
+        tipo: 'Notícia',
+        titulo: 'IBGE lança o livro “Estatísticas do Século XXI” no Encontro Diálogos IBGE 90 anos',
+        introducao: 'O livro “Estatísticas do Século XXI” foi lançado no Encontro Diálogos IBGE 90 anos, realizado nos dias 16 e 17 de novembro na Unidade de Parada Lucas (RJ) - Foto: Licia Rubinstein\\Agência IBGE Notícias O IBGE lançou, nesta quarta-feira (17), o livro “Estatísticas do...',
+        data_publicacao: '17/11/2023 20:43:00',
+        produto_id: 0,
+        produtos: '',
+        editorias: 'ibge',
+        imagens: '{"image_intro":"images\\/agenciadenoticias\\/ibge\\/2023_11\\/LivroEncerramento_THUMB.jpg","float_intro":"","image_intro_alt":"","image_intro_caption":"","image_fulltext":"images\\/agenciadenoticias\\/ibge\\/2023_11\\/LivroEncerramento_HOME.jpg","float_fulltext":"","image_fulltext_alt":"","image_fulltext_caption":""}',
+        produtos_relacionados: '',
+        destaque: true,
+        link: 'http://agenciadenoticias.ibge.gov.br/agencia-noticias/2012-agencia-de-noticias/noticias/38397-ibge-lanca-o-livro-estatisticas-do-seculo-xxi-no-encontro-dialogos-ibge-90-anos.html',
+      },
+    ],
+  };
 
+  it('1. FilterByOldest should sort news by date in ascending order when ascending is true', () => {
+    const { result } = renderHook(() => useFilter(mock.items));
     act(() => {
       result.current.filterByOldest(true);
     });
-
-    const sortedNews = [...news].sort((a, b) => {
-      const timeA = moment(a.data_publicacao, DATA);
-      const timeB = moment(b.data_publicacao, DATA);
-      return timeA.diff(timeB);
-    });
-
-    expect(result.current.filteredNews).toEqual(sortedNews);
+    expect(result.current.filteredNews).toEqual([mock.items[1], mock.items[0]]);
   });
 
   it('2. FilterByOldest should sort news by date in descending order when ascending is false', () => {
-    const { result } = renderHook(() => useFilter(news));
-
+    const { result } = renderHook(() => useFilter(mock.items));
     act(() => {
       result.current.filterByOldest(false);
     });
-
-    const sortedNews = [...news].sort((a, b) => {
-      const timeA = moment(a.data_publicacao, DATA);
-      const timeB = moment(b.data_publicacao, DATA);
-      return timeB.diff(timeA);
-    });
-
-    expect(result.current.filteredNews).toEqual(sortedNews);
+    expect(result.current.filteredNews).toEqual([mock.items[0], mock.items[1]]);
   });
 });
