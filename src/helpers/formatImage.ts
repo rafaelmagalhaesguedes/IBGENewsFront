2d904fcc17b9683611e8b1e061ffb8b9082eafed
Rafael <rafaelmagalhaes1@live.com>
2023-11-23 09:37:42 -0300

    fix: type data formatImage

--- src/helpers/formatImage.ts
+++ src/helpers/formatImage.ts
@@ -1,8 +1,9 @@
+import { NewsType } from '../types/types';
 //
 // Função que recebe um objeto com os dados de uma notícia
 // e retorna a URL da imagem formatada
 //
-export const formatImage = (data: any) => {
+export const formatImage = (data: NewsType) => {
   const images = data && JSON.parse(data.imagens || '{}');
   const url = `https://agenciadenoticias.ibge.gov.br/${images.image_intro}`;
   return url;
